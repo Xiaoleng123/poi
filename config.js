8d1ffe11aad66b00e67d9185b89185d2814c4523
xiaoleng <896054682@qq.com>
2018-05-03 20:26:27 +0800

    add files

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    database: 'business_area', // 使用哪个数据库
+    username: 'xiaoleng', // 用户名
+    password: 'xiaoleng123', // 口令
+    host: 'localhost', // 主机名
+    port: 3306 // 端口号，MySQL默认3306
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,80 @@
+const koa = require('koa');
+const app = new koa();
+const router = require('koa-router')();
+var bodyParser = require('koa-bodyparser');
+// CORS是一个W3C标准，全称是"跨域资源共享"（Cross-origin resource sharing）。
+const cors = require('koa2-cors');
+// 数据库
+const Sequelize = require('sequelize');
+const config = require('./config');
+app.use(cors({
+  origin: function (ctx) {
+      // if (ctx.url === '/test') {
+          return "*"; // 允许来自所有域名请求
+      // }
+      // return 'http://localhost:8080'; // 这样就能只允许 http://localhost:8080 这个域名的请求了
+  },
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+  maxAge: 5,
+  credentials: true,
+  allowMethods: ['GET', 'POST', 'DELETE'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}))
+
+app.use(bodyParser({
+  enableTypes:['json', 'form', 'text']
+}))
+
+router.post('/', async function (ctx, next) {
+  ctx.response.body = '跨域成功';
+});
+
+router.post('/save', async function (ctx, next) {
+  const datas = JSON.parse(ctx.request.body);
+  await saveData(datas);
+  ctx.response.body = 'save success！';
+});
+
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: 'mysql',
+  pool: {
+      max: 100,
+      min: 0,
+      idle: 30000
+  }
+});
+
+const DataBase = sequelize.define('wuhan', {
+  uid: {
+      type: Sequelize.STRING(50),
+      primaryKey: true
+  },
+  title: Sequelize.STRING(100),
+  city: Sequelize.STRING(5),
+  type: Sequelize.STRING(10),
+  address: Sequelize.STRING(255),
+  business: Sequelize.STRING(10),
+  lon: Sequelize.DECIMAL(10,6),
+  lat: Sequelize.DECIMAL(10,6),
+  phone: Sequelize.STRING(20),
+  url: Sequelize.STRING(100),
+  detailUrl: Sequelize.STRING(100),
+}, {
+  timestamps: false,
+  freezeTableName: true
+});
+
+const saveData = async function(datas) {
+  console.log('save begin······');
+  for (const data of datas) {
+    await DataBase.create(data);
+  }
+  
+  console.log('save completed!');
+};
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000);
+console.log('app started at port 3000...');
